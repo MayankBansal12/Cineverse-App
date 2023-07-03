fdd7e4edca2b8fbb59d4d6daaa66bea543c8d8dc
Mayank <mayankbansal125@gmail.com>
2023-07-04 01:00:02 +0530

    Set up TMDB Api

---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const baseURL=import.meta.env.VITE_APP_BASE_URL;
+const tmdbToken=import.meta.env.VITE_APP_TMDB_TOKEN;
+const headers={
+    Authorization: "bearer "+ tmdbToken,
+}
+export const apiFetch=async(url,params)=>{
+    try {
+        const {data}=await axios.get(baseURL+url,{
+            headers,
+            params,
+        })
+        return data;
+    } catch (error) {
+        console.log("Error in fetching",error);
+        return error;
+    }
+};
