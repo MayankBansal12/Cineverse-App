f55d7596067a8eed218b68da0aa4aa29e3ebde32
Mayank <mayankbansal125@gmail.com>
2023-07-06 21:00:15 +0530

    Design carousel for movie items

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -7,10 +7,10 @@ const Carousel = () => {
       <div class="row">
         <div class="col-lg-3 col-md-4 col-sm-6">
           <div class="movie-item">
-            <img src="https://th.bing.com/th/id/OIP.l3BZ0gvzVsw5Oe30sai9ngHaKf?pid=ImgDet&rs=1" alt="Movie Poster 1" class="img-fluid" />
+            <img src="https://th.bing.com/th/id/OIP.l3BZ0gvzVsw5Oe30sai9ngHaKf?pid=ImgDet&rs=1" alt="Movie Poster" class="img-fluid" />
             <h4 class="movie-title">Movie Title 1</h4>
             <p class="release-date"> June 1, 2022</p>
-            <p class="ratings">4.5/5</p>
+            <p class="ratings">4.5</p>
           </div>
         </div>
         <div class="col-lg-3 col-md-4 col-sm-6">
